88728b88afaf72da98be726c8264ff963b7d5d2c
Dodikki <slipslip727296@gmail.com>
2025-11-29 17:07:21 +0500

    creat ex1

--- p411-oop/p411-oop.cpp
+++ p411-oop/p411-oop.cpp
@@ -1,1 +1,64 @@
-﻿
+#include <iostream>
+
+class Employer {
+protected:
+	std::string name;
+	int year;
+    std::string lastname;
+
+public:
+	Employer(std::string n, std::string l, int y) : name(n), lastname(l), year(y) {}
+
+	virtual void Print() const = 0;
+		
+
+};
+
+
+class Prisident : Employer {
+private:
+	std::string role;
+public:
+	Prisident(std::string r, std::string n, std::string l, int y) :Employer(n,l,y),role(r){}
+
+	void Print() const override  {
+		std::cout << "Role : " << role << "\n"
+			<<"Name: " << name << "\n"
+			<< "Lastname: " << lastname << "\n"
+			<< "Year: " << year << std::endl;
+	}
+};
+class Manager : Employer {
+private:
+	std::string role;
+public:
+	Manager(std::string r, std::string n, std::string l, int y) :Employer(n, l, y), role(r) {}
+
+	void Print() const override {
+		std::cout << "Role : " << role << "\n"
+			<< "Name: " << name << "\n"
+			<< "Lastname: " << lastname << "\n"
+			<< "Year: " << year << std::endl;
+	}
+};
+
+
+class  Worker : Employer {
+private:
+	std::string role;
+public:
+	Worker(std::string r, std::string n, std::string l, int y) :Employer(n, l, y), role(r) {}
+
+	void Print() const override {
+		std::cout << "Role : " << role << "\n"
+			<< "Name: " << name << "\n"
+			<< "Lastname: " << lastname << "\n"
+			<< "Year: " << year << std::endl;
+	}
+};
+
+int main() {
+	Worker w1("Work", "Gorge", "Terov", 5);
+	w1.Print();
+
+}
