df259ed2c786e29922c3a0d3d72e1bc61e68d9d5
Dodikki <slipslip727296@gmail.com>
2025-11-30 16:38:01 +0500

    ex

--- p411-oop/p411-oop.cpp
+++ p411-oop/p411-oop.cpp
@@ -1,20 +1,26 @@
 ﻿#include <iostream>
 
 template <typename T>
-class Box {
-public:
-	T value;
-	Box(T v) : value(v) {
+T average(T numbers[], int size) {
+	
+	T sum = T();
 
-		T get() const {
-			return value;
+		for (int i, i < size, i++) {
+			sum =+ numbers[i]
+
+
 		}
-	}
-};
+		return sum / T(size);
 
+}
 
-int main() {
-	Box<int> a(10);
-	Box<std::string> b("Hello");
+int main()
+{
+	int intArr[] = { 1, 2, 3, 4, 5 };
+	double avgInt = average(intArr, 5);
+	std::cout << "Average int: " << avgInt << std::endl;
 
+	double dblArr[] = { 1.5, 2.5, 3.5 };
+	double avgDbl = average(dblArr, 3);
+	std::cout << "Average double: " << avgDbl << std::endl;
 }
