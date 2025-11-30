3f95e1633e8a51098d599661dfba703d3a4e5199
Dodikki <slipslip727296@gmail.com>
2025-11-30 16:13:31 +0500

    Box

--- p411-oop/p411-oop.cpp
+++ p411-oop/p411-oop.cpp
@@ -1,1 +1,20 @@
-﻿
+﻿#include <iostream>
+
+template <typename T>
+class Box {
+public:
+	T value;
+	Box(T v) : value(v) {
+
+		T get() const {
+			return value;
+		}
+	}
+};
+
+
+int main() {
+	Box<int> a(10);
+	Box<std::string> b("Hello");
+
+}
