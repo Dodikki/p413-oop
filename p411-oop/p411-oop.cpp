b76200f658d313a820c59e33bd69e4854e8f24e5
Dodikki <slipslip727296@gmail.com>
2025-12-06 16:07:45 +0500

    ;;[[

--- p411-oop/p411-oop.cpp
+++ p411-oop/p411-oop.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include <iostream>
+
+template <typename T>
+
+double average(T array[], int size ) {
+	if (size == 0) return 0.0;
+
+	T sum = T(0);
+	for (int i = 0; i < size; i++) 
+	{
+		sum += array[i];
+	}
+	
+	return static_cast<double> (sum) / size;
+
+
+}
+
+
+int main() {
+	double numbers[4] = { 1 ,2,4, 7 };
+
+	std::cout << average(numbers, 4);
+
+
+
+}
